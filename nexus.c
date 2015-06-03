e2821175bf12409f6bd3aca9fa213b07cac0c482
Tudor <tudor.roman@outlook.com>
2015-06-03 23:16:37 +0300

    Programul muta excelent. (L-a batut pe 4mp cu un punct :wink: ) In plus, sursa arata mai bine si foloseste mai putina memorie.

--- nexus.c
+++ nexus.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/time.h>
 
 #define TABLE_WIDTH 7
 #define DEPTH 4
 #define INFINIT 32767 // Limita pozitiva a shortului
-#define D 1 // Debug
+#define D 0 // Debug
 #define X 1
 #define O -1
 #define PLUS -2
@@ -19,9 +20,11 @@ struct TableStatus { // Structura returnata de getTableStatus()
   int currentPlayer;
 };
 
-struct TableStatus* getTableStatus( char board[TABLE_WIDTH][TABLE_WIDTH] ); // Returneaza scorul lui X, al lui O si
+void getTableStatus( char board[TABLE_WIDTH][TABLE_WIDTH],
+                    struct TableStatus *status ); // Returneaza scorul lui X, al lui O si
                                                                             // Numarul de patratele libere
-int minimax( char board[TABLE_WIDTH][TABLE_WIDTH], char depth, short alpha, short beta, char player, char computerPlayer ); // Logica jocului
+int minimax( char board[TABLE_WIDTH][TABLE_WIDTH], char depth, short alpha,
+             short beta, char player, char computerPlayer ); // Logica jocului
 inline int max( int a, int b );
 inline int min( int a, int b );
 inline int playerWon( int player, int scores[3] ); // Returneaza 1 daca player castiga, -1 daca -player castiga, 0 pt. remiza
@@ -42,7 +45,7 @@ int main() {
   FILE *fin, *fout;
   int lin, col;
   char board[TABLE_WIDTH][TABLE_WIDTH];
-  struct TableStatus *status;
+  struct TableStatus status;
   int moveLine, moveCol;
   int score, tempScore;
 
@@ -76,8 +79,9 @@ int main() {
     fgetc( stdin );
   }
   // Ia informatii despre tabla
-  status = getTableStatus(board);
-  thisPlayer = status->currentPlayer;
+  memset(&status, 0, sizeof(status));
+  getTableStatus(board, &status);
+  thisPlayer = status.currentPlayer;
   moveLine = moveCol = -1; // Nu mutam nicaieri, inca
 
   score = -INFINIT;
@@ -85,12 +89,12 @@ int main() {
     for ( col = 0; col < TABLE_WIDTH; col++) {
       if ( board[lin][col] == GOL ) {
         board[lin][col] = thisPlayer;
-        tempScore = -minimax(board, DEPTH, -INFINIT, +INFINIT, -thisPlayer, thisPlayer); // Magie
+        tempScore = - minimax(board, DEPTH, -INFINIT, +INFINIT, -thisPlayer, thisPlayer); // Magie
         board[lin][col] = GOL;
         if ( tempScore > score ) {
           score = tempScore;
           moveLine = lin;
-          moveCol = col;
+          moveCol  = col;
         }
       }
     }
@@ -140,14 +144,15 @@ inline int playerWon( int player, int board[3] ) {
     return 0;
 }
 
-struct TableStatus* getTableStatus(char board[][TABLE_WIDTH]) {
-  struct TableStatus* status = malloc(sizeof(struct TableStatus));
+void getTableStatus(char board[TABLE_WIDTH][TABLE_WIDTH],
+                                   struct TableStatus* stat ) {
+  //= malloc(sizeof(struct TableStatus));
   int scoreX, scoreO;
   int freeTiles;
   int i, j;
-  int lenSir;
+  int lenSir = 0; // Lungimea unui sir cu caractere egale
   int xPlies, oPlies;
-  int newScore[] = {0, 0, 2, 3, 10, 25, 56, 119};
+  int newScore[] = {0, 0, 0, 3, 10, 25, 56, 119};
 
   xPlies = oPlies = 0;
   scoreX = scoreO = 0;
@@ -164,6 +169,8 @@ struct TableStatus* getTableStatus(char board[][TABLE_WIDTH]) {
         } else if ( board[i][j - 1] == O ) {
           scoreO += newScore[lenSir];
           lenSir = 1;
+        } else {
+          lenSir = 1;
         }
       } else {
         lenSir = 1;
@@ -174,9 +181,9 @@ struct TableStatus* getTableStatus(char board[][TABLE_WIDTH]) {
         case GOL: freeTiles++; break;
       }
     }
-    if (board[i][j - 1] == X)
+    if (board[i][TABLE_WIDTH - 1] == X)
       scoreX += newScore[lenSir];
-    else if (board[i][j - 1] == O)
+    else if (board[i][TABLE_WIDTH - 1] == O)
       scoreO += newScore[lenSir];
     lenSir = 0;
   }
@@ -191,36 +198,36 @@ struct TableStatus* getTableStatus(char board[][TABLE_WIDTH]) {
         } else if ( board[i - 1][j] == O ) {
           scoreO += newScore[lenSir];
           lenSir = 1;
+        } else {
+          lenSir = 1;
         }
       } else {
         lenSir = 1;
       }
     }
-    if (board[i - 1][j] == X)
+    if (board[TABLE_WIDTH - 1][j] == X)
       scoreX += newScore[lenSir];
-    else if (board[i - 1][j] == O)
+    else if (board[TABLE_WIDTH - 1][j] == O)
       scoreO += newScore[lenSir];
     lenSir = 0;
   }
 
-  status->scores[O + 1] = scoreO;
-  status->scores[X + 1] = scoreX;
-  status->currentPlayer = (xPlies > oPlies ? O : X);
-  status->freeTiles = freeTiles;
-
-  return status;
+  stat->scores[O + 1] = scoreO;
+  stat->scores[X + 1] = scoreX;
+  stat->currentPlayer = (xPlies > oPlies ? O : X);
+  stat->freeTiles = freeTiles;
 }
 
 // Negamax cu alpha-beta
 // http://algopedia.ro/wiki/index.php/Note_de_curs,_clasele_9-10,_22_mai_2014#Alpha-beta
 int minimax(char board[TABLE_WIDTH][TABLE_WIDTH], char depth, short alpha, short beta, char player, char computerPlayer) {
+  int rc = 0;
   plyCounter++;
-  struct TableStatus *status = getTableStatus(board);
-  if( depth == 0 || status->freeTiles == 0 || getTime() - programStart > STOP_PROGRAM ) {
-//    return status->scores[player + 1] > status->scores[-player + 1] ? 1 :
-//           status->scores[-player + 1] > status->scores[player + 1] ? -1 : 0;
-//    return status->scores[computerPlayer + 1]
-      return playerWon(player, status->scores) * 10 - depth;
+  struct TableStatus status;
+  getTableStatus(board, &status);
+  if( depth == 0 || status.freeTiles == 0 || getTime() - programStart > STOP_PROGRAM ) {
+    rc = playerWon(player, status.scores) * 10 - depth;
+    return rc;
   }
 
   int scor = -INFINIT;
@@ -249,6 +256,6 @@ int minimax(char board[TABLE_WIDTH][TABLE_WIDTH], char depth, short alpha, short
 //      }
 //    }
 //  }
-
-  return min(alpha, beta);
+  rc = min(alpha, beta);
+  return rc;
 }
